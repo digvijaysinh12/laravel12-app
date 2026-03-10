542485638ffc35378eb54ee1ecf2df05ac92cb88
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-03-10 15:03:33 +0530

    Module 7: Implement built-in Laravel facades (Log, Cache, DB, File) with test route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +16,22 @@ Route::get('/config',[CompanyController::class, 'showCompany' ]);
 Route::get('/discount', [ProductController::class, 'discount']);
 
 Route::get('/pay',[PaymentController::class,'pay']);
+
+Route::get('/faced-test', function(){
+    Log::info("Faced");
+
+    Cache::put("name","Digvijaysinh",60);
+
+    $name = Cache::get('name');
+
+    $users = DB::table('users')->get();
+
+    $files = File::files(storage_path());
+
+    return [
+        'cached_name' => $name,
+        'users_count' => $users->count(),
+        'files_count' => count($files)
+    ];
+
+});
