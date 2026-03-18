35db38dce04b8b4a04b2058f7db7470f38cafaa3
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-03-18 15:10:03 +0530

    fix: improve product routes and add admin access control for CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,25 @@ Route::middleware('auth')->group(function () {
 
 });
 
+Route::get('/products', [ProductController::class,'index'])->name('products.index');
 
+Route::middleware(['auth','checkrole:admin'])->group(function(){
 
-Route::apiResource('products',ProductController::class);
+    Route::get('/products/create', [ProductController::class,'create'])->name('products.create');
+
+    Route::post('/products', [ProductController::class,'store'])->name('products.store');
+
+Route::get('/products/{product}/edit', [ProductController::class,'edit'])->name('products.edit');
+
+Route::put('/products/{product}', [ProductController::class,'update'])->name('products.update');
+
+Route::delete('/products/{product}', [ProductController::class,'destroy'])->name('products.destroy');
+
+Route::get('/products/{product}', [ProductController::class,'show'])->name('products.show');
+
+});
+
+Route::get('/products/{id}', [ProductController::class,'show'])->name('products.show');
+
 
 require __DIR__.'/auth.php';
