44559dee95d1e29d686cf89f7c0f43a3e6050f9d
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-03-25 10:13:10 +0530

    fix this service and improve logic of code

--- app/Services/DiscountService.php
+++ app/Services/DiscountService.php
@@ -1,9 +1,25 @@
 <?php
     namespace App\Services;
 
+    use Illuminate\Support\Facades\Log;
+
     class DiscountService{
-        public function calculateDiscount($price){
-            return $price * .30;
+        public function calculateDiscount($price, $userType = 'regular'){
+
+            Log::info('DiscountService called', [
+                'price' => $price,
+                'userType' => $userType
+            ]);
+
+            if($userType==='premium'){
+                return $price*.20; // 20% discount
+            }
+
+            if($price>5000){
+                return $price * .15;
+            }
+            
+            return $price * .05;
         }
     }
 ?>
