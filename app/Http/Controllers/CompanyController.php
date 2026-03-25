92baa71322e3094e31ce173df14f23b3b12a3273
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-03-25 15:42:49 +0530

    Module 2.1: Added CompanyController to fetch config/company values with logging and API response

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Console\Events\CommandFinished;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CompanyController extends Controller
 {
-        public function showCompany(){
-        $name = config('company.name');
-        $email = config('company.email');
+    public function showCompany()
+    {
 
-        return $name . " - " . $email;
+        Log::info('Company config accessed');
+
+        return response()->json([
+            'name' => config('company.name'),
+            'email' => config('company.email')
+        ]);
     }
 }
