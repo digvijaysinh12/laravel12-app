3475ec930ab62c2732e6fad88d9588ece927311e
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-03-25 10:13:42 +0530

    fix this service and also improvesome logic

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -1,10 +1,25 @@
 <?php
     namespace App\Services;
 
+    use Exception;
+    use Illuminate\Support\Facades\Log;
+
     class PaymentService
     {
         public function processPayment($amount){
-            return "Payment of Rs. " . $amount. " process success.";
+
+            Log::info('PayementService processing',[
+                'amount' => $amount
+            ]);
+
+            if($amount<=0){
+                throw new Exception("Invalid payment amount");
+            }
+
+            return [
+                'status' => true,
+                'message' => "Payement of Rs. $amount successful"
+            ];
         }
     }
 ?>
