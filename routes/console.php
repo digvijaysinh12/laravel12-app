0b76940bc0cb9e23e0c6a807aa4cac1176282c27
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-03-31 14:20:52 +0530

    Module 27.4 Shedule tasks

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('report:admin --type=sales')->dailyAt('02:00');
+
+Schedule::command('report:admin --type=inventory')->dailyAt('08:00');
+
+Schedule::command('log:clear')->weekly();
+
+Schedule::command('promotion:send')->dailyAt('10:00');
+
+
