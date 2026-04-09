8f13d77ae311ecceca413465c358761f10f10954
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-04-09 17:27:03 +0530

    Update API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Customer\ProductController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::middleware('throttle:60,1')->get('/products',[ProductController::class, 'apiProducts']
-);
+Route::middleware(['throttle:60,1'])
+    ->group(function () {
+        Route::get('/products', [ProductController::class, 'apiProducts'])
+            ->name('api.products.index');
+    });
