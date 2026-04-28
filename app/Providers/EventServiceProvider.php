3d3a8834ab2bc2eb8e0d7cc78ca22efd6904cf3d
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-04-28 18:22:22 +0530

    orders provider: update listener mapping

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,12 +6,12 @@ use App\Events\CartAbandoned;
 use App\Events\OrderDelivered;
 use App\Events\OrderPaid;
 use App\Events\OrderPlaced;
-use App\Events\OrderStatusUpdated;
 use App\Events\OrderShipped;
+use App\Events\OrderStatusUpdated;
 use App\Events\ProductAddedToCart;
 use App\Events\ProductReviewed;
-use App\Events\ProductViewed;
 use App\Events\ProductStockChanged;
+use App\Events\ProductViewed;
 use App\Listeners\LogOrderEvent;
 use App\Listeners\NotifyAdmin;
 use App\Listeners\NotifyUser;
@@ -25,31 +25,27 @@ use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvi
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
-        // FIXED: keep the existing event/listener map simple.
+
         OrderPlaced::class => [
             SendOrderEmail::class,
             NotifyAdmin::class,
-            NotifyUser::class,
             LogOrderEvent::class,
         ],
 
         OrderPaid::class => [
             SendOrderEmail::class,
             NotifyAdmin::class,
-            NotifyUser::class,
             LogOrderEvent::class,
         ],
 
         OrderShipped::class => [
             SendOrderEmail::class,
-            NotifyAdmin::class,
             NotifyUser::class,
             LogOrderEvent::class,
         ],
 
         OrderDelivered::class => [
             SendOrderEmail::class,
-            NotifyAdmin::class,
             NotifyUser::class,
             LogOrderEvent::class,
         ],
@@ -66,13 +62,10 @@ class EventServiceProvider extends ServiceProvider
 
         ProductAddedToCart::class => [
             NotifyAdmin::class,
-            NotifyUser::class,
             TrackAddToCart::class,
         ],
 
         CartAbandoned::class => [
-            NotifyAdmin::class,
-            NotifyUser::class,
             SendCartReminder::class,
         ],
 
