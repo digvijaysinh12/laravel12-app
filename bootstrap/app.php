48da0a53b4858f2ef3fa5fb95c3a01f490e29f0c
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-05-04 18:15:39 +0530

    add SetLocale middleware to web group

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,7 +22,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'checkrole' => \App\Http\Middleware\CheckRole::class,
         ]);
 
-
+        $middleware->web(append: [
+            \App\Http\Middleware\SetLocale::class,
+        ]);
     })
 
     ->withExceptions(function (Exceptions $exceptions): void {
