78fe3cd5fb2770be4f3aa2b1f8db48774f395306
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-05-08 16:09:18 +0530

    notifications: add lang/en/notifications.php

---
+++ lang/en/notifications.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => 'Notifications',
+    'latest_updates' => 'Latest updates and alerts',
+    'mark_all_read' => 'Mark all read',
+    'view_all' => 'View all notifications',
+    'empty' => 'You are all caught up.',
+];
