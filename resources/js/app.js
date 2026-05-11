cd0ab605f504a48bb2157fa6c62c00f2e4f4611b
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-05-11 12:08:58 +0530

    notification app js wiring

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,8 @@ import Alpine from 'alpinejs';
 import './products';
 import './cart';
 import './orders';
-import './admin';
+import './notifications/admin-notifications';
+import './notifications/customer-notifications';
 import './online-customers';
 
 window.Alpine = Alpine;
