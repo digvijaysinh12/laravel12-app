07b760442353fd85220d8f07e496ac5f4e30d8be
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2026-05-11 11:56:26 +0530

    notification user navbar component

--- app/View/Components/User/Navbar.php
+++ app/View/Components/User/Navbar.php
@@ -2,13 +2,31 @@
 
 namespace App\View\Components\User;
 
+use App\Support\Notifications\NotificationViewData;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
 class Navbar extends Component
 {
+    public function __construct(
+        private readonly NotificationViewData $notificationViewData,
+    ) {
+    }
+
     public function render(): View
     {
-        return view('user.components.navbar');
+        $notificationData = auth()->check()
+            ? $this->notificationViewData->forUser(auth()->user())
+            : [
+                'audience' => 'customer',
+                'unreadCount' => 0,
+                'latestNotifications' => [],
+            ];
+
+        return view('components.navbar.customer-navbar', [
+            'notificationAudience' => $notificationData['audience'],
+            'notificationUnreadCount' => $notificationData['unreadCount'],
+            'latestNotifications' => $notificationData['latestNotifications'],
+        ]);
     }
 }
